7a6676845b1171c71b1b1f04fbff3f3cd99d8cde
Ankit Kumar Das <76767906+ankitdasgit@users.noreply.github.com>
2024-01-02 16:14:46 +0530

    Update main.tf
    update on main file

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 ####### vpc ########
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-
 ########## subnet ##########
+
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"

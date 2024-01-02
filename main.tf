69eb1587e65fcc11e49baf7b524449d4c4a197c0
Ankit Kumar Das <76767906+ankitdasgit@users.noreply.github.com>
2024-01-02 16:12:01 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+####### vpc ########
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+########## subnet ##########
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet"
+  }
+}
+
+
+##### ec2 instance #######
+
+resource "aws_instance" "web" {
+  ami           = "ami-0bb4c991fa89d4b9b"
+  instance_type = "t3.micro"
+ subnet_id = data.aws_subnet.selected
+  tags = {
+    Name = "HelloWorld"
+  }
+}
